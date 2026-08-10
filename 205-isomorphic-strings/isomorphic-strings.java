42463d5e8dcab4b638f33091590a033bb3d85e7f
G Karthikeya <karthikeyag105@gmail.com>
2026-08-10 21:31:39 +0530

    Time: 5 ms (98.62%) | Memory: 44 MB (42.56%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length()!=t.length()) return false; 
+        int[] arr1=new int[256];
+        int[] arr2=new int[256];
+        for(int i=0;i<s.length();i++){
+            int a=s.charAt(i);
+            int b=t.charAt(i);
+            if(arr1[a]!=arr2[b]){
+                return false;
+            }
+            arr1[a]=i+1;
+            arr2[b]=i+1;
+        }
+        return true;
+        
+    }
+}
