841d32501c2e3318eaadfc19a91cae44562d3602
G Karthikeya <karthikeyag105@gmail.com>
2026-08-16 11:41:29 +0530

    Time: 8 ms (12.84%) | Memory: 114.5 MB (94.59%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        // Count stones based on remainder when divided by 3
+        for (int stone : stones) {
+            if (stone % 3 == 0) {
+                count0++;
+            } else if (stone % 3 == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        // If number of 0-remainder stones is even
+        if (count0 % 2 == 0) {
+            return count1 > 0 && count2 > 0;
+        }
+
+        // If number of 0-remainder stones is odd
+        return Math.abs(count1 - count2) >= 3;
+    }
+}
