b5d4736a4aca8a3f8460dbafe6a16a74330afcf5
G Karthikeya <karthikeyag105@gmail.com>
2026-09-11 22:24:59 +0530

    Time: 31 ms (20.48%) | Memory: 47.1 MB (10.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,30 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int left=0;
         HashMap<Character,Integer> map=new HashMap<>();
         HashMap<Character,Integer> current=new HashMap<>();
+        int left=0;
         for(int i=0;i<s1.length();i++){
-            char ch=s1.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            map.put(s1.charAt(i),map.getOrDefault(s1.charAt(i),0)+1);
         }
+        int k=s1.length();
         for(int right=0;right<s2.length();right++){
-            char ch=s2.charAt(right);
-            current.put(ch,current.getOrDefault(ch,0)+1);
-            if((right-left+1)>s1.length()){
-            char leftchar=s2.charAt(left);
-            current.put(leftchar,current.get(leftchar)-1);
-             if (current.get(leftchar) == 0) {
-                    current.remove(leftchar);
-                }
-
-            left++;
+            current.put(s2.charAt(right),current.getOrDefault(s2.charAt(right),0)+1);
+            if(right-left+1>k){
+                current.put(s2.charAt(left),current.getOrDefault(s2.charAt(left),0)-1);
+            
+            if (current.get(s2.charAt(left)) == 0) {
+                    current.remove(s2.charAt(left));
+             }
+                left++;
             }
-            if(map.equals(current)) {
+            if(map.equals(current)){
                 return true;
             }
+
         }
 
         return false;
-        
     }
 }
